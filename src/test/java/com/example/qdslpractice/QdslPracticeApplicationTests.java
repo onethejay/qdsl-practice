7efac95a424bfb092d5be1b8bacbcee6e6b475b9
onethejay <jhjang951@gmail.com>
2024-02-07 20:52:56 +0900

    Initial commit

---
+++ src/test/java/com/example/qdslpractice/QdslPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qdslpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QdslPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/example/qdslpractice/QuerydslBasicTest.java
@@ -0,0 +1,37 @@
+package com.example.qdslpractice;
+
+import com.example.qdslpractice.entity.Member;
+import com.example.qdslpractice.entity.Team;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+    @PersistenceContext
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+}
